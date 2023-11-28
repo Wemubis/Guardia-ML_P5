194bf6d5ba44d3d5fbb3162682dd881f71206355
unknown <mleboudec@gamingcampus.local>
2023-11-28 12:55:49 +0100

    change pre_check.py

--- src/pre_check.py
+++ src/pre_check.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -30,6 +31,52 @@ plt.title('Correlation Matrix')
 plt.show()
 
 # Pairplot (for small to medium-sized datasets, might take time for large datasets)
-sns.pairplot(data, hue='fraud')  # Adjust hue according to your target variable
+sampled_data = data.sample(n=1000, random_state=42)  # Sample to speed up visualization
+sns.pairplot(sampled_data, hue='fraud')  # Adjust hue according to your target variable
 plt.title('Pairplot')
 plt.show()
+
+# Distribution plots for numerical features
+numerical_cols = data.select_dtypes(include=np.number).columns.tolist()
+for col in numerical_cols:
+    plt.figure(figsize=(6, 4))
+    sns.histplot(data=data, x=col, hue='fraud', kde=True)
+    plt.title(f'Distribution of {col}')
+    plt.show()
+
+# Boxplot for numerical features against target variable
+for col in numerical_cols:
+    plt.figure(figsize=(6, 4))
+    sns.boxplot(x='fraud', y=col, data=data)
+    plt.title(f'Boxplot of {col} by fraud')
+    plt.show()
+
+# Violin plot for numerical features against target variable
+for col in numerical_cols:
+    plt.figure(figsize=(6, 4))
+    sns.violinplot(x='fraud', y=col, data=data)
+    plt.title(f'Violin Plot of {col} by fraud')
+    plt.show()
+
+# Relationship between categorical variables and target variable
+categorical_cols = data.select_dtypes(include='object').columns.tolist()
+for col in categorical_cols:
+    plt.figure(figsize=(6, 4))
+    sns.countplot(x=col, hue='fraud', data=data)
+    plt.title(f'{col} Countplot by fraud')
+    plt.xticks(rotation=45)
+    plt.show()
+
+
+# This extended version includes additional visualizations:
+
+# Distribution Plots: Shows the distribution of numerical features with respect to the target variable,
+# helpful in understanding feature distributions concerning fraud cases.
+# Boxplots: Illustrates how numerical features vary concerning the target variable.
+# Violin Plots: Similar to boxplots but displays the probability density of the data at different values.
+# Relationships with Categorical Variables: Displays count plots for categorical features with respect to the target variable.
+# These visualizations provide deeper insights into relationships between features and the target variable,
+# especially in identifying potential patterns or differences between fraudulent and non-fraudulent cases
+# across different types of features. This can guide feature selection, preprocessing steps,
+# and the choice of suitable machine learning algorithms.
+# Adjustments might be necessary based on the specific nature and size of your dataset.
